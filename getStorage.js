77d8520a11fc8f16e30934839a5bfcf161e78d3b
ltindall <ltindall@ucsd.edu>
2016-05-28 21:36:02 -0700

    Testing queries

--- getStorage.js
+++ getStorage.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
+	$.getJSON("https://rowlf.crbs.ucsd.edu:1994/api/facts/operatingsystem/Solaris",function(storageServersJson){
+		console.log(storageServersJson);
+	});  
+
+	var storageServers = []; 
+	for (i = 0; i <storageServers.length; i++){
+		storageServers.push(storageServersJson[i].certname); 
+	}
+
+	$('#fetch').html('<h2> Servers with operatingsystem = Solaris</h2>'+storageServers.toString()); 
+
+	/*
 	$.getJSON("https://rowlf.crbs.ucsd.edu:1994/api/nodes/apocalypse.crbs.ucsd.edu/facts/", function(json) {
    		console.log(json);
     	$('#fetch').html('<h2>Facts for apocalypse</h2>' + JSON.stringify(json, null, 2) );
  	});
+ 	*/
 }); 
