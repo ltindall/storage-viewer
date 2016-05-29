7d201368de4028248684e9fd3a05d30a95db18ac
ltindall <ltindall@ucsd.edu>
2016-05-28 19:06:15 -0700

    Initial commit

---
+++ getStorage.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	$.getJSON("http://localhost:8080/v3/nodes/apocalypse.crbs.ucsd.edu/facts/", function(json) {
+   		console.log(json);
+    	$('#fetch').html('<h2>Facts for apocalypse</h2>' + JSON.stringify(json, null, 2) );
+ 	});
+}
