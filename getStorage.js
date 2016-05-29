23724225fdcef16b913ed766e272cf24952d4bc9
root <root@rowlf.crbs.ucsd.edu>
2016-05-28 20:11:39 -0700

    changed url

--- getStorage.js
+++ getStorage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$.getJSON("http://localhost:8080/v3/nodes/apocalypse.crbs.ucsd.edu/facts/", function(json) {
+	$.getJSON("https://rowlf.crbs.ucsd.edu:1994/api/nodes/apocalypse.crbs.ucsd.edu/facts/", function(json) {
    		console.log(json);
     	$('#fetch').html('<h2>Facts for apocalypse</h2>' + JSON.stringify(json, null, 2) );
  	});
