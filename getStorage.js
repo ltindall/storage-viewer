ee8a39f5f6609453440eda553cf67d4ab36e87bd
root <root@rowlf.crbs.ucsd.edu>
2016-06-05 14:53:36 -0700

    Added a note for me to work on later.

--- getStorage.js
+++ getStorage.js
@@ -1,4 +1,9 @@
 
+function Comparator(a,b){
+if (a[1] < b[1]) return -1;
+if (a[1] > b[1]) return 1;
+return 0;
+}
 $(document).ready(function(){
 
   var restService = 'https://rowlf.crbs.ucsd.edu:1994/api/'; 
@@ -62,22 +67,55 @@ $(document).ready(function(){
                 //TODO: insert logic here to format size (gb, mb, kb) before adding to string 
 
                 var searchString = "zfs_space_"; 
-                var poolName = factKeys[i].substring(factKeys[i].indexOf(searchStringi)+searchString.length()); 
+                var poolName = factKeys[i].substring(factKeys[i].indexOf(searchString)+searchString.length); 
                 zfsListReport.push(poolName+": "+group.facts[factKeys[i]]+"\n"); 
 
-                poolValues.push(group.facts[factKey[i]].split(',')); 
+                poolValues.push(poolName+","+group.facts[factKeys[i]].split(',')); 
               }
 	          }
-            console.log("pool values : "+poolValues); 
+            //console.log("rpool"<"rpool/root"); 
+            poolValues.sort(Comparator); 
+            poolValues.reverse(); 
+	    //console.log(poolValues[0]); 
+            //console.log("pool values : "+poolValues);
+
+ 
             zfsListReport.sort();
             zfsListReport.reverse(); 
+            var totalSpace = zfsListReport[0].substring(zfsListReport[0].indexOf(":")+2).split(',')[1];
+            var usedSpace = 0;  
+            console.log("totalSpace ="+totalSpace); 
+	/*
+            for( var i = 0; i < zfsListReport.length; ++i){
+              
+            }
+	*/
+
+	    /*
+              TODO: 
+              Grep the zfs report for filesystems without a slash in the name. These will be the different datapools. 
+              The used + available from that first line should sum to the total for that datapool. There shouldn't be 
+              a need to go through and sum all the used values since its a tree. 
+            */ 
+
             zfsListFormatted = ""; 
             for(var i = 0; i < zfsListReport.length; ++i) {
-              zfsListFormatted += zfsListReport[i]; 
+              var zfsName = zfsListReport[i].substring(0,zfsListReport[i].indexOf(":")); 
+              var zfsLine = zfsListReport[i].substring(zfsListReport[i].indexOf(":")+2).split(','); 
+              usedSpace += Number(zfsLine[0]);
+              var zfsLineFormatted = "";  
+              for(var j = 0; j < zfsLine.length; ++j){
+                zfsLineFormatted += zfsLine[j]/1000 + " G"; 
+                if(j != zfsLine.length-1){
+                  zfsLineFormatted += ", "; 
+                }
+              }
+              zfsListFormatted += zfsName+": "+zfsLineFormatted+"\n"; 
             } 
-            console.log(zfsListPretty); 
-            console.log(zfsList); 
-
+            console.log("used space = "+usedSpace); 
+	    //console.log("zfslistformatted "+ zfsListFormatted);
+            //console.log("zfslistreport "+zfsListReport);
+		  
 	    return '<tr><td>' + group.facts.fqdn + '</td><td>'
               + group.facts.operatingsystem + '</td><td>'
               + group.facts.operatingsystemrelease + '</td><td>'
@@ -86,7 +124,7 @@ $(document).ready(function(){
               + group.facts.ipaddress + '</td><td>'
               + group.facts.productname + '</td><td>'
               + group.facts.serialnumber + '</td><td>'
-              + '<pre>'+zfsListFormatted + '</pre></td>'
+              +'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">45% Complete</span></div></div>'+ '<pre>'+"used space: "+usedSpace/1000+" G  total space: "+totalSpace/1000+" G\n"+zfsListFormatted + '</pre></td>'
                
               +'</tr>'; 
           }).join()
