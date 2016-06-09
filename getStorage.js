c107111fda4a0ae1ed2bb323fc0b73bc504d8b2a
Lucas Tindall <ltindall@ucsd.edu>
2016-06-09 10:35:03 -0700

    Fixed conversion and added rounding

--- getStorage.js
+++ getStorage.js
@@ -145,7 +145,7 @@ $(document).ready(function(){
 
                 var zfsLineFormatted = "";  
                 for(var j = 0; j < zfsLine.length; ++j){
-                  zfsLineFormatted += zfsLine[j]/1000 + " G"; 
+                  zfsLineFormatted += Math.round(zfsLine[j]/1024 * 10)/10 + " G"; 
                   if(j != zfsLine.length-1){
                     zfsLineFormatted += ", "; 
                   }
@@ -164,7 +164,7 @@ $(document).ready(function(){
                 + group.facts.ipaddress + '</td><td>'
                 + group.facts.productname + '</td><td>'
                 + group.facts.serialnumber + '</td><td>'
-                +'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">45% Complete</span></div></div>'+ '<pre>'+"used space: "+usedSpace/1000+" G  total space: "+totalSpace/1000+" G\n"+zfsListFormatted + '</pre></td>'
+                +'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">45% Complete</span></div></div>'+ '<pre>'+"used space: "+usedSpace/1024+" G  total space: "+totalSpace/1024+" G\n"+zfsListFormatted + '</pre></td>'
                  
                 +'</tr>';
             } 
