02fcb9f838373c319e12f554d956fa9bdec0aa2b
root <root@rowlf.crbs.ucsd.edu>
2016-06-09 13:19:34 -0700

    Rounded % full

--- getStorage.js
+++ getStorage.js
@@ -147,10 +147,10 @@ $(document).ready(function(){
                 
                 //var tempZfsPoolName = zfsName.substring(0,zfsName.indexOf(":")); 
 
-                if(zfsPoolName.length != 0){
-                  zfsListFormatted += "\n\n"; 
-                }
                 if(zfsName.indexOf("/") == -1){
+                  if(zfsPoolName.length != 0){
+                    zfsListFormatted += "\n\n"; 
+                  }
                   zfsPoolName = zfsName;
                   newPoolFound = true;  
                   availableSpace = zfsListReport[i].substring(zfsListReport[i].indexOf(":")+2).split(',')[1];
@@ -177,7 +177,7 @@ $(document).ready(function(){
                 }
 
                 if(newPoolFound){
-                  zfsListFormatted += "Summary for "+zfsPoolName+"-- Used Space: "+usedSpace/1024+" G  Total Space: "+totalSpace/1024+" G  Available Space: "+Number((totalSpace/1024) - (usedSpace/1024))+" G \n"+'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">'+100*(usedSpace/totalSpace)+'% Full</span>'+100*(usedSpace/totalSpace)+'% Full </div></div>'; 
+                  zfsListFormatted += "Summary for "+zfsPoolName+"-- Used Space: "+usedSpace/1024+" G  Total Space: "+totalSpace/1024+" G  Available Space: "+Number((totalSpace/1024) - (usedSpace/1024))+" G \n"+'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">'+100*(usedSpace/totalSpace)+'% Full</span>'+Math.round(100*(usedSpace/totalSpace))+'% Full </div></div>'; 
                 }
                 zfsListFormatted += zfsName+": "+zfsLineFormatted+"\n"; 
               } 
