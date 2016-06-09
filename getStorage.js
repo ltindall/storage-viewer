8a7469f8831c8b11e707ef6a7a7f2344363869b0
Lucas Tindall <ltindall@ucsd.edu>
2016-06-09 12:57:02 -0700

    Moved pre tags

--- getStorage.js
+++ getStorage.js
@@ -172,7 +172,7 @@ $(document).ready(function(){
                   }
                 }
                 if(newPoolFound){
-                  zfsListFormatted += '<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">'+100*(usedSpace/totalSpace)+'% Full</span></div></div>'+ '<pre>'+"used space: "+usedSpace/1024+" G  total space: "+totalSpace/1024+" G\n"; 
+                  zfsListFormatted += '<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">'+100*(usedSpace/totalSpace)+'% Full</span></div></div>'+"used space: "+usedSpace/1024+" G  total space: "+totalSpace/1024+" G\n"; 
                 }
                 zfsListFormatted += zfsName+": "+zfsLineFormatted+"\n"; 
               } 
@@ -188,7 +188,7 @@ $(document).ready(function(){
                 + group.facts.ipaddress + '</td><td>'
                 + group.facts.productname + '</td><td>'
                 + group.facts.serialnumber + '</td><td>'
-                + zfsListFormatted + '</pre></td>'
+                + '<pre>'+zfsListFormatted + '</pre></td>'
                 /*
                 +'<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:'+100*(usedSpace/totalSpace) +'%"><span class="sr-only">45% Complete</span></div></div>'+ '<pre>'+"used space: "+usedSpace/1024+" G  total space: "+totalSpace/1024+" G\n"+zfsListFormatted + '</pre></td>'
                  */
